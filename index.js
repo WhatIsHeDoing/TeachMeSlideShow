04cb204920f0d48b4c86e5a335da540ae65cc384
WhatIsHeDoing <darren@hickling.is>
2017-09-03 14:04:38 +0100

    Adding more animals, tweaking styling and randomising multiple images

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-Reveal.initialize();
+function randomNumberBetween(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function randomArrayElement(arr) {
+    return arr[randomNumberBetween(0, arr.length - 1)];
+}
+
+function selectRandomBackground() {
+    var selector = "data-background-image-random";
+
+    document
+        .querySelectorAll("[" + selector + "]")
+        .forEach(function(element) {
+            element.setAttribute(
+                "data-background-image",
+                randomArrayElement(element.getAttribute(selector).split(",")));
+        });
+}
+
+selectRandomBackground();
+
+Reveal.initialize({
+    loop: true,
+    mouseWheel: true,
+    viewDistance: 2
+});
