c3b2da92e8b0a5830920698a3e74cd4e6ba475c0
Darren Hickling <darrenhickling@hotmail.com>
2024-05-23 18:22:49 +0100

    🔥 Removing tslint

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -47,7 +47,6 @@ export function register(config?: Config) {
                 // Add some additional logging to localhost, pointing developers to the
                 // service worker/PWA documentation.
                 navigator.serviceWorker.ready.then(() => {
-                    // tslint:disable-next-line: no-console
                     console.log(
                         "This web app is being served cache-first by a service " +
                         "worker. To learn more, visit https://bit.ly/CRA-PWA"
@@ -78,7 +77,6 @@ function registerValidSW(swUrl: string, config?: Config) {
                             // At this point, the updated precached content has been fetched,
                             // but the previous service worker will still serve the older
                             // content until all client tabs are closed.
-                            // tslint:disable-next-line: no-console
                             console.log(
                                 "New content is available and will be used when all " +
                                 "tabs for this page are closed. See https://bit.ly/CRA-PWA."
@@ -92,7 +90,6 @@ function registerValidSW(swUrl: string, config?: Config) {
                             // At this point, everything has been precached.
                             // This is the perfect time to display a
                             // "Content is cached for offline use." message.
-                            // tslint:disable-next-line: no-console
                             console.log("Content is cached for offline use.");
 
                             // Execute callback
@@ -105,7 +102,6 @@ function registerValidSW(swUrl: string, config?: Config) {
             };
         })
         .catch(error => {
-            // tslint:disable-next-line: no-console
             console.error("Error during service worker registration:", error);
         });
 }
@@ -128,7 +124,6 @@ function checkValidServiceWorker(swUrl: string, config?: Config) {
             }
         })
         .catch(() => {
-            // tslint:disable-next-line: no-console
             console.log(
                 "No internet connection found. App is running in offline mode."
             );
